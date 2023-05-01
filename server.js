a6e291426133a010f3d9a3b166c763b1358a9f4d
na-br-wo <nathan.b.wood@gmail.com>
2023-05-01 03:21:36 -0600

    added initial get and listen methods

---
+++ server.js
@@ -0,0 +1,12 @@
+// setting up express package and initializing app variable
+const express = require('express')
+const app = express();
+const PORT = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
