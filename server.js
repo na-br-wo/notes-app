10650a62f326bac144117f877e38efa9817adabf
na-br-wo <nathan.b.wood@gmail.com>
2023-05-01 06:07:27 -0600

    connecting to static public files

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const fs = require('fs')
 const path = require('path')
 const PORT = 3001
 
+//serving the static files from ./public directory
+app.use(express.static(path.join(__dirname, 'public')))
 
-// adding GET method which will read the contents of db.json
-app.get('/', (req, res) => {
+// HTML routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+)
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+)
+
+// adding api GET method which will read the contents of db.json
+app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', (err, data) => {
     if (err) {
       console.error(err)
@@ -17,21 +27,38 @@ app.get('/', (req, res) => {
     //if not an error, parse the JSON data
     const jsonData = JSON.parse(data)
     console.log(jsonData)
-  
+
     // send data back to client
     res.json(jsonData)
   })
 })
 
-// HTML routes
-app.get('/index', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-)
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-)
+// adding api POST method for adding new note
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('Error reading database')
+    }
 
+    //if not an error, parse the JSON data
+    const jsonData = JSON.parse(data)
+    const newNote = req.body
+    // generating newNote ID using date.now() method
+    
 
+    jsonData.push(newNote)
+
+    //now writing the note data to file
+    fs.writeFile('./db/db.json', JSON.stringify(jsonData), err => {
+      if (err) {
+        console.error(err)
+        return res.status(500).send('Error adding note to database')
+      }
+      res.json(newNote)
+    })
+  })
+})
 
 app.listen(PORT, () =>
   console.log(`Notes app listening at http://localhost:${PORT}`)
