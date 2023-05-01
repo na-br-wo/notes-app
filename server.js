19df82de8f31a175ec429ab7c0a4af1dca475dad
na-br-wo <nathan.b.wood@gmail.com>
2023-05-01 04:22:15 -0600

    connected to db.json with fs read method

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 // setting up express package and initializing app variable
 const express = require('express')
 const app = express();
+const fs = require('fs')
 const PORT = 3001
 
+// adding GET method which will read the contents of db.json
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('Error reading database')
+    }
+    
+    //if not an error, parse the JSON data
+    const jsonData = JSON.parse(data)
+    console.log(jsonData)
+  
+    // send data back to client
+    res.json(jsonData)
+  })
 })
 
+
+
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Notes app listening at http://localhost:${PORT}`)
 );
